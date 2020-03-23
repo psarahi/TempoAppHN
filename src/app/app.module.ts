182b096a87162aef15f5a889826a56f3e7af2acc
Lesly Peña <p.sarahi@unitec.edu>
2020-03-23 12:30:24 -0600

    importaciones components de Ngzorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/cor
 import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
@@ -22,6 +22,37 @@ import { RegistroComponent } from './pages/registro/registro.component';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { IconsProviderModule } from './icons-provider.module';
+import { LoginComponent } from './login/login.component';
+
+// Importaciones de componentes
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+
 
 registerLocaleData(en);
 
@@ -34,6 +65,7 @@ registerLocaleData(en);
       ProgramacionProyectoComponent,
       ProyectoComponent,
       RegistroComponent,
+      LoginComponent,
    ],
    imports: [
       BrowserModule,
@@ -43,7 +75,35 @@ registerLocaleData(en);
       HttpClientModule,
       BrowserAnimationsModule,
       NgZorroAntdModule,
-      IconsProviderModule
+      IconsProviderModule,
+      ReactiveFormsModule,
+      NzCardModule,
+      NzGridModule,
+      NzLayoutModule,
+      NzAvatarModule,
+      NzTableModule,
+      NzMenuModule,
+      NzDropDownModule,
+      NzButtonModule,
+      NzIconModule,
+      NzModalModule,
+      NzDrawerModule,
+      NzFormModule,
+      NzSelectModule,
+      NzInputNumberModule,
+      NzInputModule,
+      NzSwitchModule,
+      NzMessageModule,
+      NzNotificationModule,
+      NzPageHeaderModule,
+      NzPaginationModule,
+      NzCheckboxModule,
+      NzDatePickerModule,
+      NzTimePickerModule,
+      NzEmptyModule,
+      NzListModule,
+      NzStatisticModule,
+      NzDividerModule,
    ],
    providers: [
       { provide: NZ_I18N, useValue: en_US }
