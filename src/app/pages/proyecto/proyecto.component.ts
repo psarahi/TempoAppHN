f14fa29b6619011410b1081bc44ecf12fc2e55b9
Lesly Peña <p.sarahi@unitec.edu>
2020-03-23 16:06:54 -0600

    add proyecto

--- src/app/pages/proyecto/proyecto.component.ts
+++ src/app/pages/proyecto/proyecto.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ProyectosService } from '../../Servicios/proyectos.service';
+import { ProyectoModel } from 'src/app/Modelos/proyecto';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MiembrosModel } from '../../Modelos/miembros';
+import { MiembrosService } from '../../Servicios/miembros.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-proyecto',
@@ -7,9 +14,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectoComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
+  visible = false;
+  loadingTable = true;
+  listaProtectos: ProyectoModel[];
+  listOfDisplayData: ProyectoModel[];
+  dataProyectos;
+  dataMiembros: MiembrosModel[];
+  constructor(
+    private serviceProyecto: ProyectosService,
+    private fb: FormBuilder,
+    private serviceMiembros: MiembrosService,
+    private message: NzMessageService,
+    private router: Router
 
-  ngOnInit() {
+  ) { }
+
+  createMessage(type: string, mensaje: string): void {
+    this.message.create(type, mensaje);
+  }
+
+  ngOnInit(): void {
+
+    this.serviceMiembros.getMiembros().toPromise().then(
+      (data: MiembrosModel[]) => {
+        this.dataMiembros = data;
+        // console.log(this.dataMiembros);
+
+      }
+    );
+
+    this.serviceProyecto.getProyecto().toPromise().then(
+      (data: ProyectoModel[]) => {
+        this.listaProtectos = data;
+        this.listOfDisplayData = [...this.listaProtectos];
+        this.loadingTable = false;
+      }
+    );
+    this.validateForm = this.fb.group({
+      nombreProyecto: [null, [Validators.required]],
+      responsable: [null, [Validators.required]],
+      tiempoProyectadoPro: [null, [Validators.required]],
+      presuProyectadoPro: [null, [Validators.required]],
+      estado: [null, [Validators.required]]
+    });
+  }
+
+  submitForm(): void {
+    // console.log(this.validateForm.get(['estado']).value);
+
+    // localStorage.getItem('infoUser');
+    this.dataProyectos = {
+      ...this.validateForm.value,
+      idCuenta: localStorage.getItem('infoUser'),
+      tiempoRealPro: 0,
+      presupuestoRealPro: 0
+    };
+
+    this.serviceProyecto.postProyecto(this.dataProyectos).toPromise().then(
+      (data: ProyectoModel) => {
+
+        this.listOfDisplayData = [...this.listOfDisplayData, data];
+
+        this.loadingTable = false;
+        this.createMessage('success', 'Registro creado con exito');
+
+        this.validateForm = this.fb.group({
+          nombreProyecto: [null, [Validators.required]],
+          responsable: [null, [Validators.required]],
+          tiempoProyectadoPro: [null, [Validators.required]],
+          presuProyectadoPro: [null, [Validators.required]],
+          estado: [null, [Validators.required]]
+        });
+
+      },
+      (error) => {
+        console.log(error);
+
+        this.createMessage('error', 'Opps!!! Algo salio mal');
+      }
+    );
+    this.visible = false;
+
+  }
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+
+  programacion(detalleProyecto: ProyectoModel) {
+    let navigationExtras: NavigationExtras = {
+      state: detalleProyecto
+    };
+    // console.log(navigationExtras);
+    this.router.navigate(['programacionProyectos'], navigationExtras);
   }
 
 }
