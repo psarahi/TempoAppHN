c222a2081bb205d9cf5c5e2bcc318c84b72280b6
Lesly Peña <p.sarahi@unitec.edu>
2020-03-23 12:10:50 -0600

    add paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,29 +9,48 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 
-/** config angular i18n **/
+// Importaciones de componentes
+import { CuentasComponent } from './pages/cuentas/cuentas.component';
+import { ActividadesComponent } from './pages/actividades/actividades.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { ProgramacionProyectoComponent } from './pages/programacionProyecto/programacionProyecto.component';
+import { ProyectoComponent } from './pages/proyecto/proyecto.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+
+// config angular i18n
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { IconsProviderModule } from './icons-provider.module';
-registerLocaleData(en)
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    NgZorroAntdModule,
-    IconsProviderModule
-  ],
-  providers: [
-    { provide: NZ_I18N, useValue: en_US }
-  ],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      CuentasComponent,
+      ActividadesComponent,
+      EquipoComponent,
+      ProgramacionProyectoComponent,
+      ProyectoComponent,
+      RegistroComponent,
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserModule,
+      FormsModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      NgZorroAntdModule,
+      IconsProviderModule
+   ],
+   providers: [
+      { provide: NZ_I18N, useValue: en_US }
+   ],
+   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/pages/programacionProyecto/programacionProyecto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-programacionProyecto',
+  templateUrl: './programacionProyecto.component.html',
+  styleUrls: ['./programacionProyecto.component.css']
+})
+export class ProgramacionProyectoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
