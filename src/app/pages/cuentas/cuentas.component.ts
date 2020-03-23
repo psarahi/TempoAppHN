247ec4a30a5bcce979cdecfe7fba6f595a2a4bc5
Lesly Peña <p.sarahi@unitec.edu>
2020-03-23 15:53:03 -0600

    add cuentas

--- src/app/pages/cuentas/cuentas.component.ts
+++ src/app/pages/cuentas/cuentas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CuentaModel, Cuenta } from '../../Modelos/cuenta';
+import { CuentaService } from '../../Servicios/cuenta.service';
+import * as moment from 'moment';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-cuentas',
@@ -7,9 +12,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuentasComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
+  dataCuenta;
+  loadingTable = true;
+  visible = false;
+  passwordVisible = false;
+  listaCuentas: CuentaModel[];
+  listOfDisplayData: CuentaModel[];
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private serviceCuenta: CuentaService,
+    private message: NzMessageService
+  ) { }
+
+  createMessage(type: string, mensaje: string): void {
+    this.message.create(type, mensaje);
   }
 
+  submitForm(): void {
+    // tslint:disable-next-line: forin
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+
+    this.validateForm.value.password = btoa(this.validateForm.value.password);
+    this.dataCuenta = {
+      ...this.validateForm.value,
+      fechaRegistro: moment().format('YYYY-MM-DD'),
+      perfil: '5e73eafbd981ab34a0285f42',
+    };
+
+    this.serviceCuenta.postCuenta(this.dataCuenta).subscribe(
+      (data: CuentaModel) => {
+        this.listOfDisplayData = [... this.listOfDisplayData, data];
+
+        this.loadingTable = false;
+
+        this.createMessage('success', 'Registro creado con exito');
+
+        this.validateForm = this.fb.group({
+          nombre: [null, [Validators.required]],
+          apellido: [null, [Validators.required]],
+          usuario: [null, [Validators.required]],
+          correo: [null, [Validators.email, Validators.required]],
+          password: [null, [Validators.required]],
+          empresa: [null, [Validators.required]],
+          lugar: [null, [Validators.required]],
+          estado: [null, [Validators.required]],
+        });
+
+      },
+      (error) => {
+        console.log(error);
+
+        this.createMessage('error', 'Opps!!! Algo salio mal');
+      }
+    );
+
+  }
+  ngOnInit() {
+    // debugger;
+    this.serviceCuenta.getCuenta().toPromise().then(
+      (data: CuentaModel[]) => {
+        this.listaCuentas = data;
+        this.listaCuentas.forEach(element => {
+          element.fechaRegistro = moment(element.fechaRegistro).format('YYYY-MM-DD');
+        });
+        this.listOfDisplayData = [...this.listaCuentas];
+        this.loadingTable = false;
+      }
+    );
+
+    this.validateForm = this.fb.group({
+      nombre: [null, [Validators.required]],
+      apellido: [null, [Validators.required]],
+      usuario: [null, [Validators.required]],
+      correo: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]],
+      empresa: [null, [Validators.required]],
+      lugar: [null, [Validators.required]],
+      estado: [null, [Validators.required]],
+    });
+
+  }
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
 }
