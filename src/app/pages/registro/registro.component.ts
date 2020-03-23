a8e0dc3b0859f1e6d18d0962350b1cca587bcbc0
Lesly Peña <p.sarahi@unitec.edu>
2020-03-23 16:12:14 -0600

    add registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CuentaModel, Cuenta } from '../../Modelos/cuenta';
+import { CuentaService } from '../../Servicios/cuenta.service';
+import * as moment from 'moment';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
+  dataCuenta;
+  passwordVisible = false;
+  password: string;
 
-  ngOnInit() {
+  submitForm(): void {
+    // tslint:disable-next-line: forin
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+
+    // debugger;
+    this.dataCuenta = {
+      ...this.validateForm.value,
+      fechaRegistro: moment().format('YYYY-MM-DD'),
+      perfil: 'admin',
+      estado: true
+    };
+
+    this.serviceCuenta.postCuenta(this.dataCuenta).subscribe(
+      (data) => {
+        this.createMessage('success', 'Registro creado con exito');
+      },
+      (error) => {
+        console.log(error);
+
+        this.createMessage('error', 'Opps!!! Algo salio mal');
+      }
+    );
+
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private serviceCuenta: CuentaService,
+    private message: NzMessageService
+
+  ) { }
+
+  createMessage(type: string, mensaje: string): void {
+    this.message.create(type, mensaje);
+  }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      nombre: [null, [Validators.required]],
+      apellido: [null, [Validators.required]],
+      usuario: [null, [Validators.required]],
+      correo: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]],
+      empresa: [null, [Validators.required]],
+      lugar: [null, [Validators.required]]
+    });
   }
 
 }
